c043735efa373a0c4b581267177986427271bca9
jhenrique09 <jhsv09@gmail.com>
2022-02-08 18:40:56 -0300

    SystemUIGoogle: Fix StatusBarGoogle injection and cleanup code
    Change-Id: I8269abfe7c506af15272c106c9fc085390d763ad

--- packages/SystemUI/src/org/pixelexperience/systemui/qs/tileimpl/QSFactoryImplGoogle.java
+++ packages/SystemUI/src/org/pixelexperience/systemui/qs/tileimpl/QSFactoryImplGoogle.java
@@ -131,8 +131,8 @@ public class QSFactoryImplGoogle extends QSFactoryImpl {
                 quickAccessWalletTileProvider,
 		headsupTileProvider,
                 powerShareTileProvider);
-        this.mReverseChargingTileProvider = reverseChargingTileProvider;
-        this.mBatterySaverTileGoogleProvider = batterySaverTileGoogleProvider;
+        mReverseChargingTileProvider = reverseChargingTileProvider;
+        mBatterySaverTileGoogleProvider = batterySaverTileGoogleProvider;
     }
 
     @Override
@@ -146,9 +146,9 @@ public class QSFactoryImplGoogle extends QSFactoryImpl {
 
     private QSTileImpl createTileInternal(String str) {
         if (str.equals("reverse")) {
-            return this.mReverseChargingTileProvider.get();
+            return mReverseChargingTileProvider.get();
         } else if (str.equals("battery")) {
-            return this.mBatterySaverTileGoogleProvider.get();
+            return mBatterySaverTileGoogleProvider.get();
         }
         return null;
     }
